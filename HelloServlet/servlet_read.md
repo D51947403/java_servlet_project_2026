481a1858df1770cee11bfbcde815f70a901081d9
D51947403 <singraul20codeblog@gmail.com>
2026-06-24 12:59:49 +0530

    Hello Servlet creation on eclipse

---
+++ HelloServlet/servlet_read.md
@@ -0,0 +1,15 @@
+# How to create servlet project in Eclipse
+- https://www.geeksforgeeks.org/java/creating-servlet-example-in-eclipse/
+- https://www.tpointtech.com/httpservlet-class
+- https://www.geeksforgeeks.org/java/introduction-java-servlets/
+- https://www.tutorialspoint.com/servlets/index.htm
+- https://www.geeksforgeeks.org/java/servlet-with-annotation/
+
+# To download tomcat server
+- https://tomcat.apache.org/download-90.cgi
+
+# To download any jar 
+- https://central.sonatype.com/
+
+# To download servlet-api jar 
+- https://repo1.maven.org/maven2/javax/servlet/javax.servlet-api/4.0.1/
---
+++ HelloServlet/src/main/java/HelloServlet.java
@@ -0,0 +1,133 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ * 
+ *  URL = http://localhost:8080/HelloServlet/welcome
+ */
+@WebServlet(description = "HelloServletDescription", urlPatterns = { "/welcome" })
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()  this will be called on last 
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#getServletConfig()
+	 */
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @see Servlet#getServletInfo()
+	 */
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null; 
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		 response.setContentType("text/html");
+	        
+	        PrintWriter out = response.getWriter();
+	        
+	        out.print("<html><body>");
+	        out.print("<h2>Welcome to Hello service method</h2>");
+	        out.print("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+        response.setContentType("text/html");
+        
+        PrintWriter out = response.getWriter();
+        
+        out.print("<html><body>");
+        out.print("<h2>Welcome to Hello Servlet</h2>");
+        out.print("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doHead(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doHead(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doOptions(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doOptions(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doTrace(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doTrace(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
